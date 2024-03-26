812c74393d59de8781fd775a8552093d4f638ff6
FatimaaRizwan <159219654+FatimaaRizwan@users.noreply.github.com>
2024-03-27 02:16:08 +0500

    Adding a number guessing

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber= Math.floor(Math.random()*16+1);
+
+const answer = await inquirer.prompt([
+  {name:"guessedNumber", type:"number", message:"Guess any number from 1-15"},
+]);
+
+//conditional statements
+if(answer.guessedNumber===randomNumber){
+    console.log("You guessed the right number.You won")
+}else {
+    console.log("You guessed the wrong number.You lose")
+}
